3e9620319068fb5b5a82408f66c5de26e9d65696
María Campos <marian.234cs@gmail.com>
2021-09-27 11:14:00 -0500

    agrega index a carpeta src

---
+++ src/index.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const config = require('./config');
+const authMiddleware = require('./middleware/auth');
+const errorHandler = require('./middleware/error');
+const routes = require('./routes');
+const pkg = require('../package.json');
+
+const { port, dbUrl, secret } = config;
+const app = express();
+
+// TODO: Conexión a la Base de Datos (MongoDB o MySQL)
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  // eslint-disable-next-line no-console
+  .then((db) => console.info(db.connections))
+  .catch((error) => console.info(error));
+
+app.set('config', config);
+app.set('pkg', pkg);
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(authMiddleware(secret));
+
+// Registrar rutas
+routes(app, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  app.use(errorHandler);
+
+  app.listen(port, () => {
+    console.info(`App listening on port ${port}`);
+  });
+});
